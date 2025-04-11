12abc2ec0ea0fdfb02c64f3f9cc911aa0e2dfff4
StephanieWgs <126796594+StephanieWgs@users.noreply.github.com>
2025-04-11 22:09:59 +0700

    Inisialisasi Awal untuk Backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Database connection
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.log(err));
+
+//Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
